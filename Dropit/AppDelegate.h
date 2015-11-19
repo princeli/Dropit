1db921f090547cf078b0eddc45b80810c440d338
princeli <princeli@live.cn>
2015-11-19 12:42:40 +0800

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by ly on 15/11/19.
+//  Copyright © 2015年 princeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
